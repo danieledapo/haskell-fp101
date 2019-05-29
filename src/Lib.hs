cde22926b1e88229f20d579e32c9347c3fbbddfd
Daniele D'Orazio <d.dorazio96@gmail.com>
2019-05-29 15:19:53 +0200

    step3: pure interpreter

--- src/Lib.hs
+++ src/Lib.hs
@@ -112,3 +112,76 @@ parseLoopBody s =
   let (atom , s'  ) = parseOp s
       (atoms, rest) = parseLoopBody s'
   in  (atom : atoms, rest)
+
+
+------------------------------------------------------------------------------------------
+-- Interpreter
+------------------------------------------------------------------------------------------
+
+-- | 'Tape' is an /infinite/ stream of cells that contains the state of a
+-- Brainfuck program.
+--
+-- This stream is actually made of three components: left, current and right
+-- where:
+--   * right contains all the cells to the right of current
+--   * current is the value of the current cell
+--   * left contains all the cells to the left of current in /reverse order/
+--     (that is the closest to current the closest to the start of the list)
+--
+data Tape =
+  Tape
+    { left :: [Int]
+    , current :: Int
+    , right :: [Int]
+    }
+  deriving (Show)
+
+-- | 'zeroedTape' is the starting state which is just an infinite series of 0. 
+zeroedTape :: Tape
+zeroedTape = Tape {left = repeat 0, current = 0, right = repeat 0}
+
+
+-- | 'run' all the 'Op's over the the initial 'Tape' returning the final one. 
+--
+-- There are a couple of ways to implement this in terms of 'runOp':
+--
+--   * plain recursion: manually pattern match over the list, call `runOp` on
+--     the first instruction and then call `run` again with the rest of the list
+--     and the new 'Tape'.
+--   * you might realize that 'run' is actually nothing more than a `foldl`!
+--
+-- Here are some examples
+--
+-- >>> current (run zeroedTape (parse "++<-->+-+"))
+-- 3
+--
+-- >>> current (run zeroedTape (parse "++>+++++[<+>-]<"))
+-- 7
+--
+run :: Tape -> [Op] -> Tape
+run tape ops = error "TODO"
+
+-- | 'runOp' runs only instructions that are pure (for now) over the given tape
+-- which are: 'SpInc', 'SpDec', 'CellInc', 'CellDec' and 'Loop'.
+--
+-- 'CellInc' and 'CellDec' should be quite easy to implement because they're
+-- just a matter of updating the 'current' field.
+--
+-- 'SpInc' and 'SpDec' are a bit more tricky. For example, 'SpInc' should move
+-- 'current' to 'left' and the head of 'right' into 'current' (removing it from
+-- 'right'). 'SpDec' is symmetric to 'SpInc'. Pattern matching always works,
+-- but you might find the functions 'head' and 'tail` more convinient in this
+-- case ;).
+--
+-- Lastly, 'Loop' might sound intimidating, but we've already implemented most
+-- of it. If 'current' is not 0 then it 'run' its body and recurses, otherwise
+-- it returns the state unchanged.
+--
+-- For any doubts about the syntax please check out the appropriate section in
+-- Cheatsheet.
+--
+runOp :: Tape -> Op -> Tape
+runOp _    In  = error "In is not pure, cannot implement"
+runOp _    Out = error "Out is not pure, cannot implement"
+runOp tape op  = error "TODO"
+
