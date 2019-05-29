c0ae9b6c5ce1201388a19f46ad1a469a8d37422c
Daniele D'Orazio <d.dorazio96@gmail.com>
2019-05-29 12:09:38 +0200

    step2: parser

--- src/Lib.hs
+++ src/Lib.hs
@@ -37,3 +37,67 @@ prettyOp Out         = "."
 prettyOp In          = ","
 prettyOp (Loop loop) = '[' : concatMap prettyOp loop ++ "]"
 
+
+------------------------------------------------------------------------------------------
+-- Parser
+------------------------------------------------------------------------------------------
+
+-- | 'parse' a Brainfuck program into a list of 'Op'. In case there are any
+-- unsupported symbols the parser is totally free to error out ;).
+--
+-- Here are some examples:
+--
+-- >>> parse "+-.,><[><][[]]."
+-- [CellInc, CellDec, Out, In, SpInc, SpDec, Loop [SpInc, SpDec], Loop [[]], Out]
+--
+parse :: String -> [Op]
+parse "" = []
+parse s  = let (op, rest) = parseOp s in op : parse rest
+
+-- | 'parseOp' parses the next 'Op' in a given 'String'. However, since Haskell
+-- is immutable by default we also need a way to "advance" over the input. The
+-- easiest way to do so is probably by returning the remaining input along with
+-- the parsed 'Op'.
+--
+-- For most of the instructions the implementation is pretty much
+-- straightforward, but for loops things get tricky.
+--
+-- However, the general idea is to pattern match over the first character of
+-- the input and when it's not a "[" then simply return a tuple made of the
+-- 'Op' and the rest of the string. On the other hand, when it's a "]" just
+-- delegate the actual to the `parseLoopBody` utility function that you still
+-- have to implement :).
+--
+-- Here are some examples:
+--
+-- >>> parseOp "+"
+-- (CellInc, "")
+--
+-- >>> parseOp ".>>>>>>"
+-- (Out, ">>>>>>")
+--
+-- >>> parseOp "[]"
+-- (Loop [], "")
+--
+-- >>> parseOp "[+-[]],"
+-- (Loop [CellInc, CellDec, Loop []], ",")
+--
+parseOp :: String -> (Op, String)
+parseOp s = error "TODO"
+
+
+-- | 'parseLoopBody' is meant to parse all the 'Op' inside a loop and stop when
+-- it finds the end of the loop.
+--
+-- The idea here is similar to 'parseOp':
+--
+-- * when the input starts with a "]" then the loop stops we're done and all we
+--   need to do is stop parsing the body. Pay attention that the rest of the
+--   string starts /after/ the "]".
+--
+-- * otherwise we have to parse the next op with 'parseOp', recursively call
+--   'parseLoopBody' to parse the rest of the loop and eventually combine the
+--   results.
+--
+parseLoopBody :: String -> ([Op], String)
+parseLoopBody i = error "TODO"
